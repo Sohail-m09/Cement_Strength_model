94ed2eb2fd88b97ec1e4c5cd86d49770cd2063a7
Sohail-m09 <sohailp903@gmail.com>
2026-02-10 18:14:20 +0530

    Debug which Config path is used

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,6 +3,11 @@ import pandas as pd
 import logging
 import numpy as np  
 
+
+print("DEBUG CONFIG FILE:", Config.__module__)
+print("DEBUG DATA PATH:", getattr(Config, "DATA_PATH", "NO DATA_PATH"))
+print("DEBUG FILEPATH:", getattr(Config, "filepath", "NO FILEPATH"))
+
 def data_ingestion():
     df = pd.read_csv(Config.DATA_PATH)
     return df
